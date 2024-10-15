556cd4ad408506830168dffa8a3ed3003389c60a
Samantha Nicole Petti <spetti04@d1cmp050.pax.tufts.edu>
2024-10-15 14:29:11 -0400

    adding search benchmaark code

---
+++ benchmarking/execute.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=Dhmain
+#SBATCH --partition=mpi
+#SBATCH --mem=16G
+#SBATCH --output=Dhmain.out
+#SBATCH --error=Dhmain.err
+#SBATCH --time=24:00:00
+
+config_file_name=$1
+
+source activate benchenv 
+module load cuda/12.2
+
+snakemake --rerun-incomplete --cores all --jobs 150 --restart-times 0 --latency-wait 60 \
+  --config config_file_name="$config_file_name" \
+  --cluster "sbatch --partition=mpi,batch,preempt --mem={resources.mem_mb} -c {resources.cpus} --time={resources.time} --output='$config_file_name'_slurm_output/%j.out --error='$config_file_name'_slurm_output/%j.err" \
+  --jobname "{wildcards}_{jobid}" \
+  --printshellcmds
